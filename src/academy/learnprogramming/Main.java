51111b099ec9da069b5682ae432a7c7b955f5930
Eric Lee <eelcire.eelcire@hotmail.com>
2019-09-28 17:38:42 -0500

    1.3

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,34 +3,7 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-        printDayOfTheWeek(2);
-    }
-    public static void printDayOfTheWeek(int day) {
-        switch(day) {
-            case 0:
-                System.out.println("Sunday");
-                break;
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            default:
-                System.out.println("Invalid Day");
-                break;
-        }
+        int num = 9;
+        System.out.println(9/2);
     }
 }
