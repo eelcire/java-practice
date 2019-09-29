361e7616f5aace1e44403723cb3d6beede314da9
Eric Lee <eelcire.eelcire@hotmail.com>
2019-09-29 15:56:57 -0500

    1.4

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,7 +3,7 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-        int num = 9;
-        System.out.println(9/2);
+        numberToWords(10);
     }
+
 }
